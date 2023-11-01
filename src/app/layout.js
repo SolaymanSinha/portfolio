9d6fda4783346fa7c21944a0791af66d2043bb52
Shah Solayman Sinha <md.sinha.p@gmail.com>
2023-11-01 13:30:10 +0600

    google analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import GoogleAnalytics from "./components/GoogleAnalytics";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				{process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} /> : null}
+				{children}
+			</body>
 		</html>
 	);
 }
