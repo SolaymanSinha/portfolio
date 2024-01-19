8d41d25f049b472b41961ce7d87d651dffad7df8
Shah Solayman Sinha <md.sinha.p@gmail.com>
2024-01-19 16:01:57 +0600

    under development added

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,25 @@ import AboutSection from '../components/AboutSection';
 import ProjectsSection from '../components/ProjectsSection';
 import EmailSection from '../components/EmailSection';
 import Footer from '../components/Footer';
-import AchievementsSection from '../components/AchievementsSection';
+// import AchievementsSection from '../components/AchievementsSection';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col bg-[#121212]'>
-			<Navbar />
-			<div className='container mt-24 mx-auto px-12 py-4'>
-				<HeroSection />
-				{/* <AchievementsSection /> */}
-				<AboutSection />
-				<ProjectsSection />
-				<EmailSection />
-			</div>
-			<Footer />
+		// <main className='flex min-h-screen flex-col bg-[#121212]'>
+		// 	<Navbar />
+		// 	<div className='container mt-24 mx-auto px-12 py-4'>
+		// 		<HeroSection />
+		// 		{/* <AchievementsSection /> */}
+		// 		<AboutSection />
+		// 		<ProjectsSection />
+		// 		<EmailSection />
+		// 	</div>
+		// 	<Footer />
+		// </main>
+		<main className='h-screen w-screen flex justify-center items-center'>
+			<h1 className='text-5xl'>
+				Website is under <span className='text-red-500 underline'>Development</span>
+			</h1>
 		</main>
 	);
 }
